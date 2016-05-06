c1b55f960b2b157c4d7318ffe0e426384b8f2f2b
PaulMatencio <paulo.matencio@gmail.com>
2016-05-06 07:59:24 +0200

    0.0.1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl :"/views/instructions.html"
+    })
+    .state('redBricks', {
+      url:"/redBricks",
+      templateUrl: "/views/bricks.html",
+      controller: "RedBricksCtrl as brick"
+    })
+    .state('greenBricks', {
+      url: "/greenBricks",
+      templateUrl: "/views/bricks.html",
+      controller: "greenBricksCtrl as brick"
+    })
+    .state('blueBricks', {
+      url: "/blueBricks",
+      templateUrl: "/views/bricks.html",
+      controller: "blueBricksCtrl as brick"
+    })
+    .state('cart', {
+      url:"/cart",
+      templateUrl: "/views/cart.html",
+      controller: "cartCtrl as cart"
+    });
+
+  }]);
